fc55975b42972e93c10408c4cd118210a50016d6
Parlgy <parlg4776@gmail.com>
2024-09-02 09:13:45 +0300

    WIP: Mouse trail hover effect

--- src/pages/homepage/HomePage.jsx
+++ src/pages/homepage/HomePage.jsx
@@ -13,6 +13,19 @@ import Video from "../../assets/video/videosample.png"
 
 import { FaPlay } from "react-icons/fa6";
 
+import FunIcon1 from "../../assets/images/fun/cursor.png"
+import FunIcon2 from "../../assets/images/fun/Group 5.png"
+import FunIcon3 from "../../assets/images/fun/Layer_1.png"
+import FunIcon4 from "../../assets/images/fun/path-1.png"
+import FunIcon5 from "../../assets/images/fun/path-2.png"
+import FunIcon6 from "../../assets/images/fun/path.png"
+import FunIcon7 from "../../assets/images/fun/smiley.png"
+import FunIcon8 from "../../assets/images/fun/squiggle.png"
+import FunIcon9 from "../../assets/images/fun/Vector-1.png"
+import FunIcon10 from "../../assets/images/fun/Vector.png"
+import { MouseImageTrail } from './components/MouseTrail';
+import { FiMousePointer } from "react-icons/fi";
+
 const HomePage = () => {
     // useEffect(() => {
     //     window.scrollTo(0, 0);
@@ -34,9 +47,12 @@ const HomePage = () => {
         };
     }, []);
 
+
+
+
     return (
-        <div className='max-w-screen-lg mx-auto text-[#171717] px-4'>
-            <div className='py-4 flex justify-between items-center'>
+        <div className='text-[#171717] px-4'>
+            <div className='py-4 flex justify-between items-center max-w-screen-lg mx-auto'>
                 <div className='flex items-center gap-1'>
                     <img src={Logo} alt="" />
                     <p className='text-xl helvetica-bold text-[#171717]'>Piggy Points</p>
@@ -49,7 +65,7 @@ const HomePage = () => {
                 </div>
             </div>
             <section>
-                <div className='grid lg:grid-cols-2 items-center mt-20 lg:mt-32'>
+                <div className='grid lg:grid-cols-2 items-center mt-20 lg:mt-32 max-w-screen-lg mx-auto'>
                     <div className='flex flex-col gap-2 md:w-4/5 lg:w-full mx-auto'>
                         <p className='uppercase text-center lg:text-left'>Mobile App</p>
                         <p className='text-[50px] leading-[55px] text-center lg:text-left lg:text-[60px] helvetica-bold lg:leading-[60px]'>
@@ -74,14 +90,15 @@ const HomePage = () => {
                     </div>
                 </div>
             </section>
-            <section className='pt-20 lg:pt-72 pb-32 cursor-pointer'>
+            <section className='pt-20 lg:pt-72 pb-32 cursor-pointer max-w-screen-lg mx-auto'>
                 <div className='w-full lg:h-[600px] bg-[#f4f4f4] border-[#f9f9f9] border-8 rounded-[30px] relative'>
                     <FaPlay className='absolute inset-0 mx-auto my-auto text-6xl z-10 text-white' />
                     <div className='w-full h-full absolute inset-0 bg-[#000] opacity-30 rounded-[30px]' />
                     <img src={Video} alt="" className='w-full h-full object-cover rounded-[30px]' />
                 </div>
             </section>
-            <section className='py-16 lg:py-32'>
+
+            <section className='py-16 lg:py-32 max-w-screen-lg mx-auto'>
                 <div>
                     <p className='uppercase text-center'>Why use PiggyPoints App:</p>
                     <p className='text-[50px] leading-[55px] lg:text-[60px] helvetica-bold lg:leading-[60px] text-center lg:w-2/3 mx-auto'>
@@ -91,7 +108,9 @@ const HomePage = () => {
                 </div>
                 <CardsComponent />
             </section>
-            <section className='pb-32'>
+
+
+            <section className='pb-32 max-w-screen-lg mx-auto'>
                 <p className='text-[50px] leading-[55px] lg:text-[60px] helvetica-bold lg:leading-[60px] lg:w-2/3'>
                     <span className='text-[#A6A6A6] helvetica-bold'> Our Main </span> Features
                 </p>
@@ -140,28 +159,46 @@ const HomePage = () => {
                     </div>
                 </div>
             </section>
-            <section className='py-16 lg:py-32'>
-                <div className='px-6 py-16 bg-[#f9f9f9] rounded-[30px]'>
-                    <div>
-                        <img src={Logo} alt="" className='mx-auto w-28' />
-                        <p className='text-[50px] leading-[55px] lg:text-[60px] helvetica-bold lg:leading-[70px] text-center lg:w-2/3 mx-auto'>
-                            <span className='text-[#A6A6A6] helvetica-bold'> Help Your </span> Child  {" "}
-                            <span className='text-[#A6A6A6] helvetica-bold'> Build </span> Positive
-                            <span className='text-[#A6A6A6] helvetica-bold'> Habits </span>
-                        </p>
-                        <div className='flex justify-center pt-10'>
-                            <button class="relative px-8 py-3 text-white font-bold rounded-full bg-gradient-to-r from-[#EAC870] via-[#DEA81E] to-[#BF8D10] hover:from-[#BF8D10] hover:to-[#EAC870] transform hover:scale-105 transition-all duration-300">
-                                <span class="absolute inset-0 bg-gradient-to-r from-[#EAC870] via-[#DEA81E] to-[#BF8D10] opacity-75 blur-sm rounded-full"></span>
-                                <span class="relative z-10">Join Waiting List</span>
-                            </button>
+            <MouseImageTrail
+                renderImageBuffer={50}
+                rotationRange={25}
+                images={[
+                    FunIcon1,
+                    FunIcon2,
+                    FunIcon3,
+                    FunIcon4,
+                    FunIcon5,
+                    FunIcon6,
+                    FunIcon7,
+                    FunIcon8,
+                    FunIcon9,
+                    FunIcon10,
+                ]}
+            >
+                <section className='py-16 lg:py-32'>
+                    <div className='px-6 py-16  bg-[#f9f9f9] rounded-[30px] w-full max-w-screen-lg mx-auto '>
+                        <div>
+                            <img src={Logo} alt="" className='mx-auto w-28' />
+                            <p className='text-[50px] leading-[55px] lg:text-[60px] helvetica-bold lg:leading-[70px] text-center lg:w-2/3 mx-auto'>
+                                <span className='text-[#A6A6A6] helvetica-bold'> Help Your </span> Child  {" "}
+                                <span className='text-[#A6A6A6] helvetica-bold'> Build </span> Positive
+                                <span className='text-[#A6A6A6] helvetica-bold'> Habits </span>
+                            </p>
+                            <div className='flex justify-center pt-10'>
+                                <button class="relative px-8 py-3 text-white font-bold rounded-full bg-gradient-to-r from-[#EAC870] via-[#DEA81E] to-[#BF8D10] hover:from-[#BF8D10] hover:to-[#EAC870] transform hover:scale-105 transition-all duration-300">
+                                    <span class="absolute inset-0 bg-gradient-to-r from-[#EAC870] via-[#DEA81E] to-[#BF8D10] opacity-75 blur-sm rounded-full"></span>
+                                    <span class="relative z-10">Join Waiting List</span>
+                                </button>
+                            </div>
                         </div>
                     </div>
-                </div>
-            </section>
+                </section>
+            </MouseImageTrail>
+
             <footer>
                 <p className='text-center mb-8 text-sm helvetica-regular'>©️ Copyright Piggy Points App 2024 | All Rights Reserved</p>
             </footer>
-        </div>
+        </div >
     )
 }
 
